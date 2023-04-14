683fa1b738ca9555b143619f308e55d5a083d2ec
Elthon <elthonferreira18@gmail.com>
2023-04-13 23:03:49 -0300

    Adição dos arquivos de configuração após instalação do node jasmine e karma no projeto

--- spec/testeSpec.js
+++
@@ -1,9 +0,0 @@
-describe("Testes (specs)", function () {
-	it ("Deve garantir que o valor booleano 'true' é 'true'", function () {
-
-	});
-
-	it ("Deve garantir que o valor booleano 'true' não é 'false'", function () {
-
-	});
-});
